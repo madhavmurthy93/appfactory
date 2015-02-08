c0371588dff96cd17a56accdfde66260309b6512
ktgow <ktgow-github@growf.com>
2015-02-08 14:51:57 -0800

    Reverted homepage.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var cool = require('cool-ascii-faces');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    //res.render('index', { title: 'AppFactory' });
-    response.send(cool());
+  res.render('index', { title: 'AppFactory' });
 });
 
 module.exports = router;
