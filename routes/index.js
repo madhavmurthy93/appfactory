a62a5166aca846ff46b272a1c89930aef2103980
ktgow <ktgow-github@growf.com>
2015-02-08 14:49:49 -0800

    Testing alternate homepage.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var cool = require('cool-ascii-faces');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AppFactory' });
+    //res.render('index', { title: 'AppFactory' });
+    response.send(cool());
 });
 
 module.exports = router;
