d89de39083d12a3e71966bae310c9a00feb963b6
ktgow <ktgow-github@growf.com>
2015-02-08 20:26:09 -0800

    Grabbing test MySql data.

--- routes/index.js
+++ routes/index.js
@@ -2,29 +2,29 @@ var express = require('express');
 var router = express.Router();
 var mysql      = require('mysql');
 
-var connection = mysql.createConnection({
-    host     : process.env.MYSQL_HOST,
-    user     : process.env.MYSQL_USER,
-    password : process.env.MYSQL_PW,
-    database : process.env.MYSQL_DB
-});
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var mysql_connected = false;
+    var connection = mysql.createConnection({
+	host     : process.env.MYSQL_HOST,
+	user     : process.env.MYSQL_USER,
+	password : process.env.MYSQL_PW,
+	database : process.env.MYSQL_DB
+    });
     connection.connect();
 
-    connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+    connection.query('SELECT id, name FROM users', function(err, rows, fields) {
+	connection.end()
+	data = { title: 'AppFactory',
+		 mysql_connected: false,
+		 users: []}
 	if (err) {
 	    console.log('MySql error: ', err);
-	    mysql_connected = 'Nope'
+	    data.mysql_connected = 'Nope'
 	} else {
-	    console.log('The solution is: ', rows[0].solution);
-	    mysql_connected = rows[0].solution;
+	    data.users = rows;
 	}
-	res.render('index', { title: 'AppFactory',
-			      mysql_connected: mysql_connected });
+	res.render('index', data);
     });
 });
 
