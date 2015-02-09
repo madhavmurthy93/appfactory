87a50e78b2157b386e122647b998432ddcad039c
ktgow <ktgow-github@growf.com>
2015-02-08 20:11:56 -0800

    More MySql testing.

--- routes/index.js
+++ routes/index.js
@@ -16,16 +16,16 @@ router.get('/', function(req, res, next) {
     connection.connect();
 
     connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-	if (err) throw err;
-
-	console.log('The solution is: ', rows[0].solution);
-	mysql_connected = rows[0].solution;
+	if (err) {
+	    console.log('MySql error: ', err);
+	    mysql_connected = 'Nope'
+	} else {
+	    console.log('The solution is: ', rows[0].solution);
+	    mysql_connected = rows[0].solution;
+	}
+	res.render('index', { title: 'AppFactory',
+			      mysql_connected: mysql_connected });
     });
-
-    connection.end();
-
-    res.render('index', { title: 'AppFactory',
-			  mysql_connected: mysql_connected });
 });
 
 module.exports = router;
