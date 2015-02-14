8ac59e0f8c4e3aaed5c700058b01f054ade70655
madhavmurthy93 <madhavmurthy93@yahoo.com>
2015-02-14 14:36:27 -0800

    Connection ended code.

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ router.get('/about', function(req, res, next) {
 });
 
 router.post('/about', function(req, res, next) {
+	// connect to the mysql database
 	connection_config = {
 	host     : process.env.MYSQL_HOST,
 	user     : process.env.MYSQL_USER,
@@ -20,15 +21,18 @@ router.post('/about', function(req, res, next) {
 	database : process.env.MYSQL_DB
     }
 
+    /* insert user comment into the mysql database and redirect
+   	 * to the home page.
+   	 */
     var connection = mysql.createConnection(connection_config);
     connection.connect();
     var entry = {name: req.body.name, email: req.body.email, comment: req.body.comment};
     connection.query('INSERT INTO contact_us_emails SET ?', entry, function(err, result) {
+    	connection.end();
     	if (err) {
     		console.log(err);
-    	} else {
-    		res.redirect('/about');
     	}
+    	res.redirect('/about');
     });
 });
 
