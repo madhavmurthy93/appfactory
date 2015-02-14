74367021bf6318d5ff9c43223ca46ece631f4684
madhavmurthy93 <madhavmurthy93@yahoo.com>
2015-02-14 14:11:56 -0800

    Emails and comments now get added to the database.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
 
 /* GET home page. */
@@ -11,8 +12,24 @@ router.get('/about', function(req, res, next) {
 	res.render('about');
 });
 
-router.post('/post', function(req, res, next) {
+router.post('/about', function(req, res, next) {
+	connection_config = {
+	host     : process.env.MYSQL_HOST,
+	user     : process.env.MYSQL_USER,
+	password : process.env.MYSQL_PW,
+	database : process.env.MYSQL_DB
+    }
 
+    var connection = mysql.createConnection(connection_config);
+    connection.connect();
+    var entry = {name: req.body.name, email: req.body.email, comment: req.body.comment};
+    connection.query('INSERT INTO contact_us_emails SET ?', entry, function(err, result) {
+    	if (err) {
+    		console.log(err);
+    	} else {
+    		res.redirect('/about');
+    	}
+    });
 });
 
 module.exports = router;
