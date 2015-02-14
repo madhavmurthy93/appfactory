2cc1b6e1cc41baec5fb88f6e43c9d67882eb63db
ktgow <deus-github@growf.com>
2015-02-14 14:52:05 -0800

    Total mess, but uploading a thumbnail image for an idea works now.

--- routes/idea.js
+++ routes/idea.js
@@ -11,6 +11,35 @@ router.get('/', function(req, res) {
 });
 
 
+// Find an unused Idea ID from the SQL database, and return it.
+var GetNextAvailableIdeaId = function(callback) {
+    connection_config = {
+	host     : process.env.MYSQL_HOST,
+	user     : process.env.MYSQL_USER,
+	password : process.env.MYSQL_PW,
+	database : process.env.MYSQL_DB
+    }
+    var connection = mysql.createConnection(connection_config);
+    connection.connect();
+
+    connection.query(
+	'SELECT id FROM ideas ORDER BY id DESC LIMIT 1',
+	function(err, rows) {
+	    connection.end();
+	    if (err) {
+		console.log('MySql error:', err);
+		throw err;
+	    }
+	    var nextId = 0;
+	    if (rows.length > 0) {
+		nextId = rows[0].id + 1;
+	    }
+	    
+	    callback(nextId);
+	});
+};
+
+
 // This route receives the posted form.
 // As explained above, usage of 'body-parser' means
 // that `req.body` will be filled in with the form elements
@@ -27,40 +56,43 @@ router.post('/',
 		// Read the image contents and save them to the database.
 		//var readStream = fs.createReadStream(
 		//    req.files.displayImage.buffer);
-		var ideaId = 1;
+		GetNextAvailableIdeaId(function(ideaId) {
+		    console.log('Set ideaId to', ideaId);
 
-		connection_config = {
-		    host     : process.env.MYSQL_HOST,
-		    user     : process.env.MYSQL_USER,
-		    password : process.env.MYSQL_PW,
-		    database : process.env.MYSQL_DB
-		}
-		console.log('Creating connection...');
-		var connection = mysql.createConnection(connection_config);
-		console.log('Connecting...');
-		connection.connect();
+		    connection_config = {
+			host     : process.env.MYSQL_HOST,
+			user     : process.env.MYSQL_USER,
+			password : process.env.MYSQL_PW,
+			database : process.env.MYSQL_DB
+		    }
+		    console.log('Creating connection...');
+		    var connection = mysql.createConnection(connection_config);
+		    console.log('Connecting...');
+		    connection.connect();
 
-		connection.query(
-		    'INSERT INTO ideas (id, thumbnail) ' +
-			'VALUES (?,?)', [ideaId, req.files.displayImage.buffer],
-		    function(err) {
-			console.log('Insert done:', err);
-			if (err) {
-			    console.log('MySql error: ', err);
-			    var err = new Error('Database error: ' + err);
-			    err.status = 500;
-			    next(err);
-			} else {
-			    var imagePath = req.files.displayImage.path;
-			    var imageName = req.files.displayImage.name;
+		    connection.query(
+			'INSERT INTO ideas (id, thumbnail) ' +
+			    'VALUES (?,?)', [ideaId, req.files.displayImage.buffer],
+			function(err) {
+			    connection.end();
+			    console.log('Insert done:', err);
+			    if (err) {
+				console.log('MySql error: ', err);
+				var err = new Error('Database error: ' + err);
+				err.status = 500;
+				next(err);
+			    } else {
+				var imagePath = req.files.displayImage.path;
+				var imageName = req.files.displayImage.name;
 
-			    console.log('Post handler.', req.files)
-			    var html = 'Hello ' + req.body.name + '.<br>' +
-				'<img src="/idea/thumbs/' + ideaId + '"><br>' +
-				'<a href="/idea">Try again.</a>';
-			    res.send(html);
-			}
-		    });
+				console.log('Post handler.', req.files)
+				var html = 'Hello ' + req.body.name + '.<br>' +
+				    '<img src="/idea/thumbs/' + ideaId + '"><br>' +
+				    '<a href="/idea">Try again.</a>';
+				res.send(html);
+			    }
+			});
+		});
 	    });
 
 
@@ -80,22 +112,15 @@ router.get('/thumbs/*', function(req, res) {
     connection.connect();
 
     console.log('Querying DB...');
-    connection.query('SELECT thumbnail FROM ideas',
+    connection.query('SELECT thumbnail FROM ideas WHERE id=?', [imageName],
 		     function(err, rows, fields) {
 			 console.log('Results:', err, rows, fields);
-			 connection.end()
+			 connection.end();
 			 console.log('Row length:', rows.length);
 			 if (rows.length == 1) {
 			     res.send(rows[0].thumbnail);
 			 }
 		     });
-
-    /*
-    var imagePath = './idea_images/' + imageName;
-
-    var readStream = fs.createReadStream(imagePath);
-    readStream.pipe(res);
-    */
 });
 
 module.exports = router;
