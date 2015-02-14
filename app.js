f93975613f3036c8ede1c7cf2a698278e46e7c21
ktgow <ktgow-github@growf.com>
2015-02-13 23:38:04 -0800

    Messing around with file uploads and form submission.

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var index = require('./routes/index');
+var idea = require('./routes/idea');
 var testdb = require('./routes/testdb');
 
 var app = express();
@@ -22,7 +23,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', index);
+app.use('/idea', idea);
 app.use('/testdb', testdb);
 
 // catch 404 and forward to error handler
---
+++ routes/idea.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var fs = require('fs');
+var multer = require('multer');
+var router = express.Router();
+
+
+/* GET idea page. */
+router.get('/', function(req, res) {
+    res.render('idea');
+});
+
+
+// This route receives the posted form.
+// As explained above, usage of 'body-parser' means
+// that `req.body` will be filled in with the form elements
+router.post('/',
+	    multer({dest: './idea/',
+		    limits:{fileSize: 1024*1024,
+			    files: 1},
+		    onFileUploadComplete: function (file) {
+			console.log(file.fieldname + ' uploaded to  '
+				    + file.path);
+		    }}),
+	    function(req, res){
+		var imagePath = req.files.displayImage.path;
+		var imageName = req.files.displayImage.name;
+		/*
+		var readStream = fs.createReadStream(
+		    imagePath);
+		readStream.pipe(res);
+		*/
+
+		console.log('Post handler.', req.files)
+		var html = 'Hello ' + req.body.name + '.<br>' +
+		    '<img src="/idea/thumbs/' + imageName + '"><br>' +
+		    '<a href="/idea">Try again.</a>';
+		    res.send(html);
+
+	    });
+
+
+router.get('/thumbs/*', function(req, res) {
+    console.log('Thumbs image:', req.path.split('/')[2]);
+    res.send('');
+});
+
+module.exports = router;
