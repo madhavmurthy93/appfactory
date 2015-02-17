0bbacd99f5bc3bc330092431b7a521c0e8daface
ktgow <ktgow-github@growf.com>
2015-02-16 17:30:52 -0800

    Change auth.js to use simplified SQL library.

--- routes/auth.js
+++ routes/auth.js
@@ -2,76 +2,53 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport')
 	, FacebookStrategy = require('passport-facebook').Strategy;
-var mysql = require('mysql');
+var sql = require('../util/sql');
 
 passport.serializeUser(function(user, done) {
 	done(null, user);
 });
 
 passport.deserializeUser(function(user, done) {
-	// connect to the mysql database
-	connection_config = {
-	host     : process.env.MYSQL_HOST,
-	user     : process.env.MYSQL_USER,
-	password : process.env.MYSQL_PW,
-	database : process.env.MYSQL_DB
-    }
-
-    var connection = mysql.createConnection(connection_config);
-    connection.connect();
-    connection.query('SELECT * FROM users WHERE id = ?', [user.id], function(err, rows) {
-    	connection.end();
-    	if (!err) {
-    		done(null, rows[0]);
-    	} else {
-    		done(err, null);
-    	}
-    });
+    sql.SimpleQueryPromise('SELECT * FROM users WHERE id = ?', [user.id])
+	.then(function(rows) {
+	    done(null, rows[0]);
+	}, function(err) {
+	    done(err, null);
+	});
 });
 
+
 passport.use(new FacebookStrategy({
-	clientID: '857739407615807',
-	clientSecret: '188e497c70b716dfed42e5d680a4ec41',
-	callbackURL: 'https://uw-app-factory.herokuapp.com/auth/facebook/callback'
+	clientID: process.env.FB_CLIENTID,
+	clientSecret: process.env.FB_CLIENT_SECRET,
+	callbackURL: 'https://uw-app-factory.heroku.com/auth/facebook/callback'
 	},
 	function(accessToken, refreshToken, profile, done) {
-		// connect to the mysql database
-		connection_config = {
-		host     : process.env.MYSQL_HOST,
-		user     : process.env.MYSQL_USER,
-		password : process.env.MYSQL_PW,
-		database : process.env.MYSQL_DB
-	    }
-
-	    var connection = mysql.createConnection(connection_config);
-	    connection.connect();
-	    connection.query('SELECT * FROM users WHERE id = ?', [profile._json.id], function(err, rows) {
-	    	if (err) {
-	    		console.log(err);
-	    	}
-	    	if (!err && rows.length != 0) {
+	    sql.SimpleQueryPromise('SELECT * FROM users WHERE id = ?',
+				   [profile._json.id])
+		.then(function(rows) {
+	    	    if (rows.length != 0) {
 	    		connection.end();
 	    		done(null, rows[0]);
-	    	} else {
-	    		var user = {id: profile._json.id, name: profile._json.name, description: ''};
-	    		connection.query('INSERT INTO users SET ?', user, function(err) {
-	    			connection.end();
-	    			if (err) {
-	    				console.log(err);
-	    			} else {
-	    				done(null, user);
-	    			}
-	    		});
-	    	}
-
-	    });
+	    	    } else {
+	    		var user = {id: profile._json.id,
+				    name: profile._json.name, description: ''};
+			sql.SimpleQueryPromise('INSERT INTO users SET ?', user)
+			    .then(function() {
+				done(null, user);
+			    }, function(err) {
+				console.log(err);
+			    });
+		    }
+		});
 	}
 ));
 
 router.get('/facebook', passport.authenticate('facebook'));
 
-router.get('/facebook/callback', passport.authenticate('facebook', {failureRedirect: '/about'}), function(req, res) {
-	res.redirect('/');
-});
+router.get('/facebook/callback',
+	   passport.authenticate('facebook',
+				 {successRedirect: '/',
+				  failureRedirect: '/about'}));
 
 module.exports = router;
