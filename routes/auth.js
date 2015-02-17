303bc058d34949765b1d0c6131228c08f30ccd90
ktgow <deus-github@growf.com>
2015-02-16 19:52:25 -0800

    Changed auth.js to use an environment variable for the callback.  Added comments.jade.

--- routes/auth.js
+++ routes/auth.js
@@ -21,7 +21,7 @@ passport.deserializeUser(function(user, done) {
 passport.use(new FacebookStrategy({
 	clientID: process.env.FB_CLIENTID,
 	clientSecret: process.env.FB_CLIENT_SECRET,
-	callbackURL: 'https://uw-app-factory.heroku.com/auth/facebook/callback'
+	callbackURL: process.env.FB_CALLBACK_URL
 	},
 	function(accessToken, refreshToken, profile, done) {
 	    sql.SimpleQueryPromise('SELECT * FROM users WHERE id = ?',
