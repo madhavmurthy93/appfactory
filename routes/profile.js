f73ec74d6879a50d03492f13a954141cfe36fc5f
ktgow <ktgow-github@growf.com>
2015-02-19 03:12:07 -0800

    Added isDeveloper checkbox to profile page.  Updates server via AJAX.  Cleaned up unnecessary script from layout.jade.

--- routes/profile.js
+++ routes/profile.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
     }
     var userId = res.locals.user.id;
 
-    sql.SimpleQueryPromise('SELECT id, name, description, is_Developer '
+    sql.SimpleQueryPromise('SELECT id, name, description, is_developer '
 			   + 'FROM users WHERE id=?', [userId])
 	.then(function(rows) {
 	    if (rows.length != 1) {
@@ -19,11 +19,31 @@ router.get('/', function(req, res, next) {
 
 	    user = rows[0];
 	    res.render('profile', {user: user});
-	    data = { title: 'AppFactory',
-		     mysql_connected: true,
-		     users: rows}
 	}).catch(function(err) {
 	    console.log('Profile error: ', err);
+	    res.status(500);
+	    res.render('error', {
+		message: err.message,
+		error: {}
+	    });
+	});
+});
+
+
+// Set whether the user is a developer or not.
+router.post('/setDeveloper', function(req, res, next) {
+    if (!res.locals.user) {
+	throw new Error('You must be logged in to modify your profile.');
+    }
+
+    var userId = res.locals.user.id;
+
+    sql.SimpleQueryPromise('UPDATE users SET is_developer=? WHERE id=?',
+			   [req.body.isDeveloper, userId])
+	.then(function() {
+	    res.send('');
+	}).catch(function(err) {
+	    console.log('setDeveloper error: ', err);
 	    res.status(500);
 	    res.render('error', {
 		message: err.message,
