0f6dade0f5610595d60a91eeab40be3bdaa43d76
ktgow <deus-github@growf.com>
2015-02-22 11:59:02 -0800

    Added a page for hosting fake screenshots.

--- routes/index.js
+++ routes/index.js
@@ -24,4 +24,21 @@ router.get('/', function(req, res, next) {
 	});
 });
 
+
+// Fake screenshot pages.
+router.get('/fake:fakeId', function(req, res, next) {
+    var fakeId = parseInt(req.params.fakeId);
+    var fakeIdMap = {
+	0: 'WelcomeToProject.png',
+	1: 'FakeSourceRepository.png',
+	2: 'FakeSourceRepository.png'
+    };
+    var imageName = fakeIdMap[fakeId] || 'FakeSourceRepository.png';
+
+    res.send('<html><body><a href="fake' + (fakeId + 1)
+	     + '"><img src="images/' + imageName + '"></a>'
+	     + '</body></html>');
+});
+
+
 module.exports = router;
